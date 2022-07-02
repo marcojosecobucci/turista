a8e434e26640d8ce475c18fdf3b268ab197464ff
Marco Cobucci <marco.cobucci@outlook.com>
2022-07-02 10:57:35 +0200

    create class

---
+++ src/DB/Database.java
@@ -0,0 +1,25 @@
+package DB;
+
+import Booking.Booking;
+import Feedback.Feedback;
+import House.House;
+import User.Client.Client;
+import User.Host.Host;
+
+import java.util.HashSet;
+
+public class Database {
+    private HashSet<Client> clientsList;
+    private HashSet<Host> hostsList;
+    private HashSet<House> housesList;
+    private HashSet<Booking> bookingsList;
+    private HashSet<Feedback> feedbacksList;
+
+    public Database() {
+        clientsList = new HashSet<>();
+        hostsList = new HashSet<>();
+        housesList = new HashSet<>();
+        bookingsList = new HashSet<>();
+        feedbacksList = new HashSet<>();
+    }
+}
